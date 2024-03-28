47c5cc3faaef40a6da0fc3a5a7afbf30d7ce5d82
gyuwon <103185451+gimgyuwon@users.noreply.github.com>
2024-03-28 19:51:25 +0900

    [Silver III] Title: 1로 만들기, Time: 216 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,28 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
     static Integer[] dp;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        dp = new Integer[N + 1];
-        dp[0] = dp[1] = 0;
-        System.out.print(recur(N));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(recur(N,0));
     }
-    static int recur(int N) {
-        if (dp[N] == null) {
-            if (N % 6 == 0) {
-                dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
-            } else if (N % 3 == 0) {
-                dp[N] = Math.min(recur(N/3), recur(N-1)) + 1;
-            } else if (N % 2 == 0) {
-                dp[N] = Math.min(recur(N/2), recur(N-1)) + 1;
-            } else {
-                dp[N] = recur(N - 1) + 1;
-            }
+    static int recur(int N, int count) {
+        if (N < 2){
+            return count;
         }
-        return dp[N];
+        return Math.min(recur(N/2, count + 1 + (N%2)), recur(N/3, count + 1 + (N%3)));
     }
 }
