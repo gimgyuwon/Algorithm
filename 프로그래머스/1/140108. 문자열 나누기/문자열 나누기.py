20be2fbb5c889a257adf1c7082c4c45e4be357eb
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2024-10-30 07:54:00 -0700

    [level 1] Title: 문자열 나누기, Time: 3.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,21 +1,26 @@
+from collections import deque
+
 def solution(s):
-    x = s[0]
-    same_cnt, diff_cnt = 0, 0
-    answer = 0
-    for i in range(len(s)):
-        if s[i] == x:
-            same_cnt += 1
-        else:
-            diff_cnt += 1
-            
-        if same_cnt == diff_cnt:
-            answer += 1
-            if i+1 < len(s):
-                x = s[i+1]
-            same_cnt, diff_cnt = 0, 0
+    ans = 0
+    q = deque(s)
     
-    if same_cnt > 0 or diff_cnt > 0:
-        answer += 1
-            
-    return answer
-            
+    while q:
+        a, b = 1, 0
+        x = q.popleft()
+        
+        while q:
+            n = q.popleft()
+            if n == x:
+                a += 1
+            else:
+                b += 1
+
+            if a == b:
+                ans += 1
+                break
+        else:
+            # q가 비어 있고 a != b 인 경우 마지막으로 추가
+            if a != b:
+                ans += 1
+
+    return ans
