4248bea2fa6fefb7ab4994b9b00ed85ddc24e3b4
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2024-11-07 19:51:56 -0700

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 33164 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,65 @@
+import sys
+
+
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, value):
+        self.top = Node(value, self.top)
+
+    def pop(self):
+        if not self.top:
+            return -1
+        else:
+            node = self.top
+            self.top = node.next
+            return node.val
+
+    def size(self):
+        if not self.top:
+            return 0
+        else:
+            node = self.top
+            size = 0
+            while node:
+                size += 1
+                node = node.next
+            return size
+
+    def empty(self):
+        if not self.top:
+            return 1
+        else:
+            return 0
+
+    def peek(self):
+        if not self.top:
+            return -1
+        else:
+            return self.top.val
+
+
+cmd_num = int(sys.stdin.readline().strip())
+cmd_lst = [sys.stdin.readline().strip() for _ in range(cmd_num)]
+
+stack = Stack()
+
+for cmd in cmd_lst:
+    if cmd.startswith("push"):
+        _, val = cmd.split()
+        stack.push(int(val))
+    elif cmd == "pop":
+        print(stack.pop())
+    elif cmd == "size":
+        print(stack.size())
+    elif cmd == "empty":
+        print(stack.empty())
+    else:
+        print(stack.peek())
