58102fc2efdcd8bbd6b9e8f28fc7637ac234e46e
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2024-11-09 18:29:26 -0700

    [Bronze II] Title: 단어순서 뒤집기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
@@ -3,8 +3,7 @@ import sys
 
 def solution(input_data):
     for i, part in enumerate(input_data, start=1):
-        part.reverse()
-        print("Case #{}:".format(i), str(part)[1:-1].replace('\'', '').replace(',', ''))
+        print(f"Case #{i}: {' '.join(part[::-1])}")
 
 
 input_num = int(sys.stdin.readline().strip())
