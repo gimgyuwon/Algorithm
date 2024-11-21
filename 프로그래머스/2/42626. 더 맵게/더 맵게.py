14fadc2a82838c4b7125b82c48b89ef946523b3e
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2024-11-21 14:19:56 -0700

    [level 2] Title: 더 맵게, Time: 1867.70 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -4,15 +4,15 @@ import heapq
 def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
-    part = heapq.heappop(scoville)
-    while part < K:
+    while True:
+        part = heapq.heappop(scoville)
+        if part >= K:
+            return answer
         if len(scoville) == 0:
             return -1
         new_part = part + heapq.heappop(scoville)*2
         heapq.heappush(scoville, new_part)
-        part = heapq.heappop(scoville)
         answer += 1
-    return answer
 
 scoville = [1, 2, 3, 9, 10, 12]
 K = 7
