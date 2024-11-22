6c1e13f99999210f52dbed39d05aec81db55251d
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2024-11-21 21:24:25 -0700

    [Silver V] Title: K번째 수, Time: 3332 ms, Memory: 631660 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.py
+++ 백준/Silver/11004. K번째 수/K번째 수.py
@@ -1,16 +1,26 @@
 import sys
-import heapq
+import random
 
 
-def solution(k, num_list):
-    heapq.heapify(num_list)
-    for _ in range(int(k)-1):
-        heapq.heappop(num_list)
-    return heapq.heappop(num_list)
+def quickselect(arr, k):
+    if len(arr) == 1:
+        return arr[0]
+
+    pivot = random.choice(arr)
+    lows = [x for x in arr if x < pivot]
+    highs = [x for x in arr if x > pivot]
+    pivots = [x for x in arr if x == pivot]
+
+    if k < len(lows):
+        return quickselect(lows, k)
+    elif k < len(lows) + len(pivots):
+        return pivots[0]
+    else:
+        return quickselect(highs, k - len(lows) - len(pivots))
 
 
 input_data = sys.stdin.readline().strip()
-num, k = input_data.split()
+num, k = map(int, input_data.split())
 num_list = list(map(int, sys.stdin.readline().strip().split()))
 
-print(solution(k, num_list))
+print(quickselect(num_list, k - 1))
