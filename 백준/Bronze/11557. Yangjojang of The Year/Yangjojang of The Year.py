708c9508c225911db1a751da084fd860caa864b0
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2024-11-25 14:20:33 -0700

    [Bronze I] Title: Yangjojang of The Year, Time: 52 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -2,11 +2,11 @@ import sys
 from collections import Counter
 
 def solution(test_data):
-    sorted_data = sorted(test_data,  key=lambda x: int(x[1]), reverse=True)
-    return sorted_data[0]
+    max_data = max(test_data, key=lambda x:int(x[1]))
+    return max_data[0]
 
 test_num = int(sys.stdin.readline().strip())
 for _ in range(test_num):
     test_case_num = int(sys.stdin.readline().strip())
     test_data = [sys.stdin.readline().strip().split(" ") for _ in range(test_case_num)]
-    print(solution(test_data)[0])
+    print(solution(test_data))
