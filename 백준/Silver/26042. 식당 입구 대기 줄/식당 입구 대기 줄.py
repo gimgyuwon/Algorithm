83314b63f78781a1fb5707eb6ca3d72cfbe0eefa
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2024-12-24 09:36:11 -0700

    [Silver V] Title: 식당 입구 대기 줄, Time: 160 ms, Memory: 65940 KB -BaekjoonHub

--- 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.py
+++ 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.py
@@ -1,33 +1,30 @@
-from collections import deque
 import sys
+from collections import deque
 
-def max_queue_size(n, A):
-    queue = deque()  # 줄을 나타내는 큐
-    max_count = 0  # 대기 중인 학생 수의 최댓값
-    last_student = None  # 최대 학생 수일 때 맨 뒤에 있는 학생 번호
+def solution(N, data):
+    line = []
+    max_line  = 0
+    result = []
 
-    for command in A:
-        if command[0] == 1:  # 학생이 줄을 서는 경우
-            student_id = command[1]
-            queue.append(student_id)
-            # 최대 대기 인원 수 갱신
-            if len(queue) > max_count:
-                max_count = len(queue)
-                last_student = student_id
-            elif len(queue) == max_count:
-                last_student = min(last_student, student_id)
+    for item in data:
+        action = int(item[0])
+        a = item[1] if len(item) > 1 else None
+        if action == 1:
+            line.append(a)
+        elif action == 2:
+            if line:
+                line.pop()
+        if max_line <= len(line):
+            max_line = len(line)
+            last_std = line[-1] if line else None
+            result.append([max_line, last_std])
 
-        elif command[0] == 2:  # 식사 1인분 준비로 학생이 줄에서 나가는 경우
-            queue.popleft()
+    filtered = [arg for arg in result if arg[0] == max_line]
+    result = min(filtered, key=lambda x: int(x[1]))
 
-    print(max_count, last_student)
+    return f"{result[0]} {result[1]}"
 
-# 입력 처리
+
 n = int(sys.stdin.readline().strip())
-A = []
-
-for _ in range(n):
-    line = list(map(int, sys.stdin.readline().strip().split()))
-    A.append(line)
-
-max_queue_size(n, A)
+input_data = [sys.stdin.readline().strip().split(" ") for _ in range(n)]
+print(solution(n, input_data))
