92a417a92df6596fb5d076659a21522039e9af83
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2025-01-23 20:47:04 -0700

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 612 ms, Memory: 97872 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,30 +1,31 @@
 import sys
+from collections import defaultdict
+
 sys.setrecursionlimit(10**6)
+input = sys.stdin.read
 
 def dfs(graph, node, visited, seq, order):
     visited[node] = True
     seq[node] = order[0]
     order[0] += 1
-    for i in sorted(graph[node]):
-        if not visited[i]:
-            dfs(graph, i, visited, seq, order)
-    return seq
+    for neighbor in sorted(graph[node]):
+        if not visited[neighbor]:
+            dfs(graph, neighbor, visited, seq, order)
 
+data = input().splitlines()
+n, m, r = map(int, data[0].split())
+edges = data[1:]
 
-n, m, r = map(int, sys.stdin.readline().strip().split(" "))
-edges = [list(map(int, sys.stdin.readline().strip().split(" "))) for _ in range(m)]
-graph = [[] for _ in range(n+1)]
-
+graph = defaultdict(list)
 for edge in edges:
-    u, v = edge
+    u, v = map(int, edge.split())
     graph[u].append(v)
     graph[v].append(u)
 
-visited = [False] * (n+1)
-seq = [0] * (n+1)
+visited = [False] * (n + 1)
+seq = [0] * (n + 1)
 order = [1]
 
 dfs(graph, r, visited, seq, order)
 
-for i in range(1, n+1):
-    print(seq[i])
+sys.stdout.write("\n".join(map(str, seq[1:])) + "\n")
