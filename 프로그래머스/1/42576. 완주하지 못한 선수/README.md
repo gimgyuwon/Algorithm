c734642fe678549998a20739c7d8a9f987fa0a62
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2026-04-03 16:54:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.02 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 57.6 MB, 시간: 64.55 ms
+메모리: 66.4 MB, 시간: 42.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 11일 18:49:44
+2026년 04월 03일 16:54:30
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,19 +1,16 @@
 function solution(participant, completion) {
-    const pMap = new Map();
-    
-    for (const p of participant){
-        pMap.set(p, (pMap.get(p) || 0) + 1)
+    const pMap = new Map()
+    for (const name of participant){
+        pMap.set(name, (pMap.get(name) || 0) + 1)
     }
     
-    // console.log(pMap)
-    
-    for (const p of completion){
-        pMap.set(p, (pMap.get(p) || 0) - 1)
+    for (const name of completion){
+        pMap.set(name, pMap.get(name) -1)
     }
     
-    for (const [k,v] of pMap){
-        if (v !== 0){
-            return k
+    for (const [name, cnt] of pMap){
+        if (cnt > 0){
+            return name
         }
     }
 }
