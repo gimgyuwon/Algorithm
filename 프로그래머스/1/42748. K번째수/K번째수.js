8168ed80aad3b29267fe4fab59e234493c8ed355
Gwen Kim <103185451+gimgyuwon@users.noreply.github.com>
2026-04-03 15:25:48 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,8 @@
 function solution(array, commands) {
-    let ans = []
-    for (const cmd of commands){
-        const filterLst = array.slice(cmd[0]-1, cmd[1]).sort((a,b) => a-b)
-        ans.push(filterLst[cmd[2]-1])
-        // console.log(filterLst, cmd, ans)
+    const ans = []
+    for (const [i,j,k] of commands){
+        ans.push(array.slice(i-1,j).sort((a,b) => a-b)[k-1])
     }
+    
     return ans
 }
